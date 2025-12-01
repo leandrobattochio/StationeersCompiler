f9a4f67015a44fffdd46d0c7c1cbdf6960fdc652
Leandro Battochio <lbattochio@hotmail.com>
2025-12-01 00:41:09 -0300

    code optimization refactor to strategy + pipeline pattern

---
+++ Compiler.Core/CodeGeneration/Strategies/BinOpMoveOptimize.cs
@@ -0,0 +1,33 @@
+﻿using Compiler.Domain.IR;
+
+namespace Compiler.CodeGeneration.Strategies;
+
+public class BinOpMoveOptimize : IOptimizationStrategy
+{
+    public IrProgram Apply(IrProgram program)
+    {
+        for (var i = 0; i < program.Instructions.Count - 1; i++)
+        {
+            if (program.Instructions[i] is BinaryOpInstruction binOpInstr &&
+                program.Instructions[i + 1] is MoveInstruction moveInstr)
+            {
+                // cmd target left right
+                // sub r2 r0 2733.15
+
+                // cmd 
+                // mov r0 r2
+
+                if (binOpInstr.Target == moveInstr.Source && binOpInstr.Left == moveInstr.Target)
+                {
+                    program.Instructions[i] = new BinaryOpInstruction(binOpInstr.Left, binOpInstr.Left,
+                        binOpInstr.Right, binOpInstr.Op);
+
+                    // Remove the move instruction
+                    program.Instructions.RemoveAt(i + 1);
+                }
+            }
+        }
+
+        return program;
+    }
+}
---
+++ Compiler.Core/CodeGeneration/Strategies/IOptimizationStrategy.cs
@@ -0,0 +1,8 @@
+﻿using Compiler.Domain.IR;
+
+namespace Compiler.CodeGeneration.Strategies;
+
+public interface IOptimizationStrategy
+{
+    IrProgram Apply(IrProgram program);
+}
